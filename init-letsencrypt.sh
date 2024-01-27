bd17f3f1eb9adb69543855eec7f7f397bf070271
root <root@2455294-cb77567.twc1.net>
2024-01-27 15:55:42 +0300

    feat: end dev config for get cert

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -2,7 +2,7 @@
 
 domains=("dev.lorexiq.ru www.dev.lorexiq.ru")
 email="murashko100500@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 data_path="./certbot"
 rsa_key_size=4096
